c45681b40758d4ea2355796295d6dd11eb099f79
furkanyaylacesme <furkanyaylacesme@gmail.com>
2023-02-22 16:36:53 +0300

    hesap makinesi tamalandı

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double sonuc;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Birinci sayı:");
+        double birinci_sayi = input.nextDouble();
+        System.out.print("İkinci sayı:");
+        double ikinci_sayi = input.nextDouble();
+
+        System.out.println("1: +\n2: -\n3: *\n4: /");
+        System.out.print("Seçiminiz:");
+        int operator = input.nextInt();
+
+        switch (operator) {
+            case 1:
+                sonuc = birinci_sayi + ikinci_sayi;
+                System.out.print("Sonuç:" + sonuc);
+                break;
+            case 2:
+                sonuc = birinci_sayi - ikinci_sayi;
+                System.out.print("Sonuç:" + sonuc);
+                break;
+            case 3:
+                sonuc = birinci_sayi * ikinci_sayi;
+                System.out.print("Sonuç:" + sonuc);
+                break;
+            case 4:
+                if (ikinci_sayi != 0) {
+                    sonuc = birinci_sayi / ikinci_sayi;
+                    System.out.print("Sonuç:" + sonuc);
+                }
+                else{
+                        System.out.println("Sayı sıfıra bölünemez.");
+                    }
+                break;
+            default:
+                        System.out.println("Yanlış seçim yaptınız tekrar deneyiniz.");
+
+
+                }
+        }
     }
-}
